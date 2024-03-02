49692cd68802e73f730215bacea9436a2051e950
After3ffects <121523105+After3ffects@users.noreply.github.com>
2024-03-02 16:21:46 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -27,4 +27,42 @@ const updateUserCount = () => {
 
 updateUserCount();
 
+const finishCreateJobForm = () => {
+    document.body.classList.toggle('activeForm');
+    const title = (document.getElementById("createJobTitle")).value;
+    const description = (document.getElementById("createJobDescription")).value;
+    const amount = (document.getElementById("createJobAmount")).value;
+    const email = (document.getElementById("createJobEmail")).value;
+    console.log(title);
+    console.log(description);
+    console.log(amount);
+    console.log(email);
+    const newJobObject = document.createElement("div");
+    newJobObject.setAttribute("class", "oddJobObject");
+    
+    const objectTitle = document.createElement("h3");
+    const titleText = document.createTextNode(title);
+    objectTitle.appendChild(titleText);
+
+    const objectDescription = document.createElement("p");
+    const descriptionText = document.createTextNode(description);
+    objectDescription.appendChild(descriptionText);
+
+    const objectAmount = document.createElement("p");
+    const amountText = document.createTextNode(amount);
+    objectAmount.appendChild(amountText);
+
+    const objectEmail = document.createElement("p");
+    const emailText = document.createTextNode(email);
+    objectEmail.appendChild(emailText);
+
+    newJobObject.appendChild(objectTitle);
+    newJobObject.appendChild(objectDescription);
+    newJobObject.appendChild(objectAmount);
+    newJobObject.appendChild(objectEmail);
+
+    document.getElementById("content").appendChild(newJobObject);
+
+    console.log(newJobObject);
+}
 
