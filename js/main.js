b12727e1ee9a5f2117b8d706a81ad6c86d1e2800
After3ffects <121523105+After3ffects@users.noreply.github.com>
2024-03-02 12:42:32 -0500

    added header styling

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ async function updateQuote() {
     const res = await fetch('./CowQuotes.json')
 
     quoteData = await res.json();
-    quoteData= quoteData["Quotes"]
-    let quote = quoteData[Math.floor(Math.random() * (quoteData.length+1))]
-    document.getElementById("navQuote").innerHTML = quote
+    quoteData= quoteData["Quotes"];
+    let index = Math.floor(Math.random() * (quoteData.length));
+    let quote = quoteData[index];
+    document.getElementById("headerQuote").innerHTML = quote;
+    console.log(index);
 }
 
 updateQuote();
