ec55b238332af7c2b4b543d3d5b6e492cd3732c1
After3ffects <121523105+After3ffects@users.noreply.github.com>
2024-03-02 12:11:15 -0500

    added random quotes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-const updateQuote = () => {
-    
+async function updateQuote() {
+    let quoteData;
+
+    const res = await fetch('./CowQuotes.json')
+
+    quoteData = await res.json();
+    quoteData= quoteData["Quotes"]
+    let quote = quoteData[Math.floor(Math.random() * (quoteData.length+1))]
+    document.getElementById("navQuote").innerHTML = quote
 }
+
+updateQuote();
